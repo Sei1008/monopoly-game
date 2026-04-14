81e2c01ab86a96098252c3e8530bb10b78769e4e
Thanh Trúc <truc03062007@gmail.com>
2026-04-14 16:24:04 +0700

    first commit

--- src/config/GameConfig.java
+++ src/config/GameConfig.java
@@ -1,5 +1,12 @@
 package config;
 
 public class GameConfig {
-    
+   int startingmoney;
+   int maxPlayers;
+   int boardSize; 
+   public void loadConfig() {
+    this.startingmoney = 1500;
+    this.maxPlayers = 4;
+    this.boardSize = 40;
+}
 }
--- src/service/GameService.java
+++ src/service/GameService.java
@@ -1,5 +1,23 @@
 package service;
+import config.GameConfig;
+import java.util.ArrayList;
+import java.util.List;
+import model.Board;
+import model.Dice;
+import model.Player;
 
 public class GameService {
+     Board board;
+    List<Player> players = new ArrayList<>();
+    Dice dice;
+    int currentPlayer;
+   public void initGame(GameConfig config) {
+       players = new ArrayList<>();
+        board = new Board();   
+        config.loadConfig();
+        dice= new Dice();
+
+    }
     
+
 }
