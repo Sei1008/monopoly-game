2c4cbbfedcd18b2682eab81958da429e6fd82342
hao nhien <nhien3043@gmail.com>
2026-04-14 15:57:28 +0700

    fix: correct typos and utility rent in Board

--- src/model/Board.java
+++ src/model/Board.java
@@ -10,7 +10,7 @@ public class Board {
     public void initializeBoard(){
         squares[0] = new Square("GO",SquareType.GO);
         squares[1] = new Square("Mediterranean Ave",SquareType.PROPERTY,new Property("Mediterranean Ave", 60, 2, "brown"));
-        squares[2] = new Square("Community Chess",SquareType.COMMUNITY_CHEST);
+        squares[2] = new Square("Community Chest",SquareType.COMMUNITY_CHEST);
         squares[3] = new Square("Baltic Ave",SquareType.PROPERTY,new Property("Baltic Ave", 60, 4, "brown"));
         squares[4] = new Square("Income Tax",SquareType.TAX);
         squares[5] = new Square("Reading Railroad",SquareType.PROPERTY,new Property("Reading Railroad", 200, 25, "railroad"));
@@ -18,9 +18,9 @@ public class Board {
         squares[7] = new Square("Chance",SquareType.CHANCE);
         squares[8] = new Square("Vermont Ave",SquareType.PROPERTY,new Property("Vermont Ave", 100, 6, "light blue"));
         squares[9] = new Square("Connecticut Ave",SquareType.PROPERTY,new Property("Connecticut Ave",120,8,"light blue"));
-        squares[10] = new Square("JAIl",SquareType.JAIL);
+        squares[10] = new Square("Jail",SquareType.JAIL);
         squares[11] = new Square("St. Charles Pl", SquareType.PROPERTY, new Property("St. Charles Pl", 140, 10, "pink"));
-        squares[12] = new Square("Electric Co", SquareType.PROPERTY, new Property("Electric Co", 150, 0, "utility")); 
+        squares[12] = new Square("Electric Co", SquareType.PROPERTY, new Property("Electric Co", 150, 4, "utility")); 
         squares[13] = new Square("States Ave", SquareType.PROPERTY, new Property("States Ave", 140, 10, "pink"));
         squares[14] = new Square("Virginia Ave", SquareType.PROPERTY, new Property("Virginia Ave", 160, 12, "pink"));
         squares[15] = new Square("Pennsylvania RR", SquareType.PROPERTY, new Property("Pennsylvania RR", 200, 25, "railroad"));
@@ -36,7 +36,7 @@ public class Board {
         squares[25] = new Square("B&O Railroad", SquareType.PROPERTY, new Property("B&O Railroad", 200, 25, "railroad"));
         squares[26] = new Square("Atlantic Ave", SquareType.PROPERTY, new Property("Atlantic Ave", 260, 22, "yellow"));
         squares[27] = new Square("Ventnor Ave", SquareType.PROPERTY, new Property("Ventnor Ave", 260, 22, "yellow"));
-        squares[28] = new Square("Water Works", SquareType.PROPERTY, new Property("Water Works", 150, 0, "utility")); 
+        squares[28] = new Square("Water Works", SquareType.PROPERTY, new Property("Water Works", 150, 4, "utility")); 
         squares[29] = new Square("Marvin Gardens", SquareType.PROPERTY, new Property("Marvin Gardens", 280, 24, "yellow"));
         squares[30] = new Square("Go To Jail", SquareType.GO_JAIL); 
         squares[31] = new Square("Pacific Ave", SquareType.PROPERTY, new Property("Pacific Ave", 300, 26, "green"));
