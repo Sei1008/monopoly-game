0aafd32fff76b913add771ff0241baf0e945a6a9
Ha My <hamy.hct@gmail.com>
2026-05-06 09:30:13 +0700

    UI

---
+++ src/FXML_Controllers/Communitychest_page.java
@@ -0,0 +1,30 @@
+package FXML_Controllers;
+
+import application.Main;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class Communitychest_page extends Main implements Initializable {
+
+    @FXML private Label PopUp_Label;
+    @FXML private Button Button_OK;
+
+    @Override
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+        PopUp_Label.setText(Main.Static_PopUp_Label.getText());
+    }
+
+    @FXML
+    private void Click_On_OK(MouseEvent event) {
+        closeWindow(event);
+    }
+
+    private void closeWindow(MouseEvent event) {
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+}
